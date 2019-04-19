d55f467010321de0fdcb71b5e12f7631c53a0c12
aurelbnn <49645033+aurelbnn@users.noreply.github.com>
2019-04-19 04:27:23 +0200

    endgame screen

--- pages/scripts/game.js
+++ pages/scripts/game.js
@@ -511,9 +511,35 @@ function rightDirBall2(){
          ballFired2 = false;
        }
 }
+/*
+var endGame
 
+processor.doLoad = function doLoad() {
+  if (player1.life < 0){
+    this.video = document.querySelector(`${}Death);
+  }
+  if (player2.life < 0){
+    this.video = document.querySelector('jonSnowDeath');
 
+  }
+}
 
+var processor;
+
+  processor.doLoad = function doLoad() {
+    this.video = document.getElementById('video');
+    this.c1 = document.getElementById('c1');
+    this.ctx1 = this.c1.getContext('2d');
+    this.c2 = document.getElementById('c2');
+    this.ctx2 = this.c2.getContext('2d');
+    let self = this;
+    this.video.addEventListener('play', function() {
+        self.width = self.video.videoWidth / 2;
+        self.height = self.video.videoHeight / 2;
+        self.timerCallback();
+      }, false);
+  },
+*/
 window.addEventListener("keydown", controller2.keyListener);
 window.addEventListener("keyup", controller2.keyListener);
 window.requestAnimationFrame(loopPlayer2);
